908dd0bb3256d3df49f7fdcdff569fef5742e6ff
mohammedbilal <>
2024-02-18 11:49:31 +0530

    Updated

--- hypr/net.sh
+++ hypr/net.sh
@@ -2,7 +2,7 @@
 HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
 
 hyprctl --batch "$HYPRCMDS"
-hyprctl dispatch exec [workspace 1 silent] nemo /home/mohammedbilalns/Documents/Texts
+hyprctl dispatch exec [workspace 1 silent] dolphin /home/mohammedbilalns/Documents/Texts
 hyprctl dispatch exec [workspace 2 silent] code /home/mohammedbilalns/Documents/GitHub/CSIR-UGC_notes_mathematics 
 hyprctl dispatch exec [workspace 1 silent] firefox 
 
