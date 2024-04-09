85ae20942696bae5230cb40481f74731e0079ad5
Mohammed Bilal N S <bilalnsmuhammed@gmail.com>
2024-04-09 10:52:37 +0530

    Updated script tidies hypr config

--- postinstall.sh
+++ postinstall.sh
@@ -1,23 +1,82 @@
-#!/usr/bin/env bash
+ #!/bin/bash
 
+# Installing base-devel
 sudo pacman -S --needed base-devel 
-echo "installing paru..."
-git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si && cd $HOME
-echo "installing some packages from repo..."
-paru -Sy atuin nautilus firefox telegram-desktop ntfs-3g bluez bluez-utils blueman  waybar wofi hyprland hyrplock hyprpaper hypridle ttf-space-mono-nerd ttf-jetbrains-mono neovim python-pynvim npm nwg-look texlive qt6ct kvantum zsh foot xdg-user-dirs neofetch htop brightnessctl kate pamixer mako  qt6-5compat qt6-declarative qt6-svg sddm qt5-quickcontrols qt5-quickcontrols2 qt5-graphicaleffects gnome-keyring gtk-engine-murrine sassc gnome-themes-extra gwenview tree-sitter-cli hyprlock hypridle papirus-icon-theme pipewire pipewire-jack pipewire-media-session pipewire-pulse pipewire-alsa  gnome-disk-utility qbittorrent zathura zathura-pdf-poppler yazi bat cups cups-pdf  
-echo "installing some packages from aur..."
-paru -S github-desktop-bin pamac-aur xfce-polkit hyprshade rnote tlp tlp-rdw sddm-conf-git joplin canon-pixma-ts5055-complete
 
-systemctl enable bluetooth.service
-systemctl enable tlp.service
-systemctl enable cups.service
-echo "generating hyprland config ..."
+#Installing paru 
+if ! command -v paru  &>/dev/null 
+then
+	echo "installing paru..."
+	git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si && cd $HOME
+fi
+ 
+# list of applications
+apps=("$@")
+predefined_apps=( "telegram-desktop" "zathura" "atuin" "zathura-pdf-poppler" "firefox" "ntfs-3g"  "nwg-look" "texlive" "zsh" "xdg-usr-dirs" "neofetch" "htop" "bat" "yazi" "zathura" "zathura-pdf-poppler" "qbitorrent" "gnome-disk-utility" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-media-session" "pipewire-jack"  "gwenview" "bluez" "bluez-utils" "npm" "rnote-bin" "neovim" "python-pynvim" "sassc" "gnome-keyring" "gtk-engine-murrine" "sassc""gnome-themes-extra" "tree-sitter-cli" "papirus-icon-theme" "github-desktop-bin" "pamac-aur" "tlp" "tlp-rdw" "joplin-bin"  )
+
+sddm_configuration=("qt6-declarative" "qt6-svg" "sddm" "qt5-quick-controls" "qt5-quickcontrols2" "qt5-graphicaleffects" "sddm-conf-git"  )
+
+hyprland_packages=("waybar" "hyprland-git" "wofi" "hyprlock" "hypridle" "hyprpaper" "hyprshade" "ttf-space-mono-nerd" "ttf-jetbrains-mono""qt5ct" "kvantum" "foot" "brightnessctl" "pamixer" "mako" "blueman" "nwg-look"  "kvantum" "xfce-polkit"  "cliphist" "nwg-clipman")
+
+printing=( "cups" "cups-pdf" "canon-pixma-ts5055-complete")
+apps+=("${predefined_apps[@]}")
+
+sddm_check=0
 git clone https://github.com/mohammedbilalns/Arch--Hyperland-rice.git
-cp -r $HOME/Arch--Hyperland-rice/* $HOME/.config/
-echo "creating sddm theme..."
-sudo cp -r $HOME/Arch--Hyperland-rice/sddm/sugar-candy /usr/share/sddm/themes/
+echo -n "Do you want to install sddm configurations? (yes/no)"
+read response
+if [[ $response =~ ^[Yy][Ee][Ss]$ ]] || [[ $response =~ ^[Yy]$ ]]
+then 
+	apps+=("${sddm_configuration[@]}")
+	echo "copying sddm theme"
+	sudo cp -r Arch--Hyperland-rice/sddm /usr/share/sddm/themes/
+	sddm_check=1
+elif [[ $response =~ ^[Nn][Oo]$ ]] || [[ $response =~ ^[Nn]$ ]]
+then
+	 continue
+else
+	echo "Invalid response"
+	exit 2
+fi
+echo -n "Do you want to install hyprland packages? (yes/no)"
+read response
+if [[ $response =~ ^[Yy][Ee][Ss]$ ]] || [[ $response =~ ^[Yy]$ ]]
+then 
+	apps+=("${hyprland_packages[@]}")
+	echo "copying hyprland configurations... "
+	cp -r  Arch--Hyperland-rice/* $HOME/.config/
+
+elif [[ $response =~ ^[Nn][Oo]$ ]] || [[ $response =~ ^[Nn]$ ]]
+then
+	continue
+else
+	echo "Invalid response."
+	exit 2
+fi
+
+echo -n "Do you want to install packages for printing? (yes/no)"
+read response
+if [[ $response =~ ^[Yy][Ee][Ss]$ ]] || [[ $response =~ ^[Yy]$ ]]
+then 
+	apps+=("${printing[@]}")
+	systemctl enable cups.service
+elif [[ $response =~ ^[Nn][Oo]$ ]] || [[ $response =~ ^[Nn]$ ]]
+then
+	continue
+else
+	echo "Invalid response."
+	exit 2
+fi
 
 
-
-
+echo "Installing necessary packages"
+for app in "${apps[@]}"; do
+    paru  -S --noconfirm "$app"
+done
+ systemctl enable bluetooth.service 
+ systemctl enable tlp.service
+if [ sddm_check==1 ]
+then 
+	echo "Change sddm theme through sddm-conf-git"
+fi
 
